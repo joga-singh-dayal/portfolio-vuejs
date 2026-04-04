b61cb0b75f2ccea72a63ca396f59472ad90be346
Joga Singh Dayal <joga-singh@web.de>
2026-04-04 19:37:01 +0200

    fix: added analytics

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+    <Analytics />
     <RouterView />
 </template>
 
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
 import { RouterView } from 'vue-router'
 </script>
